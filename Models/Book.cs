beef4b904b310f8b4dc094733125e362767e1334
xayotbek <hayotbek2525@gmail.com>
2024-03-27 02:28:45 +0500

    book.cs fayli yaratildi va banchga qoshildi

---
+++ Models/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masala.Models
+{
+    public class Book
+    {
+        private string _muallifi;
+        private decimal _narxi;
+        public string Muallifi
+        { 
+            get { return _muallifi; }
+            set { _muallifi = value; } 
+        }
+        public decimal Narxi
+        {
+            get {return _narxi;}
+            set {_narxi = value;}
+        }
+        private string _kitobNomi;
+        public Book(string kitobNomi)
+        {
+            _kitobNomi = kitobNomi;
+        }
+
+        public void PrintKitobNomi()
+        {
+            System.Console.WriteLine($"Kitob nomi: {_kitobNomi}");
+        }
+        public void PrintKitobMuallif()
+        {
+            System.Console.WriteLine($"Kitob muallifi: {Muallifi}");
+        }
+        public void PrintKitobNarxi()
+        {
+            System.Console.WriteLine($"Kitob narxi: {Narxi}");
+        }
+
+        public void PrintAll()
+        {
+            System.Console.WriteLine($"Kitob nomi: {_kitobNomi}\nKitob muallifi: {Muallifi}\nKitob narxi: {Narxi}");
+        }
+    }
+}
